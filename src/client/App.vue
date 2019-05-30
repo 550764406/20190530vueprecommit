3046f65830d390bbaf3e9064be9ec29de24db6f1
duanliwei <duanliwei@51kxd.cn>
2019-05-30 14:19:52 +0800

    'test'

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,23 +1,53 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png" />
-    <router-view />
-  </div>
+	<div id="app">
+		<img src="./assets/logo.png" />
+		<router-view />
+	</div>
 </template>
 
 <script>
 export default {
-  name: "App"
+	name: 'App',
+
+	data() {
+		return {
+			msg: 'Welcome to Your Vue.js App',
+		};
+	},
+	methods: {
+		myclick() {
+			/**
+			 *
+			 *
+			 *
+			 *
+			 *
+			 *
+			 *
+			 *
+			 *
+			 *
+			 *
+			 *
+			 *
+			 *
+			 *
+			 *                                */
+			(aa) => {
+				console.log('');
+			};
+		},
+	},
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
--- src/server/app.js
+++ src/server/app.js
@@ -8,11 +8,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // 访问静态资源
 app.use(express.static(path.resolve(__dirname, '../../dist')));
 // 访问单页
-app.get('*', function (req, res) {
-  var html = fs.readFileSync(path.resolve(__dirname, '../../dist/index.html'), 'utf-8');
-  res.send(html);
+app.get('*', function(req, res) {
+	var html = fs.readFileSync(path.resolve(__dirname, '../../dist/index.html'), 'utf-8');
+	res.send(html);
 });
 // 监听
-app.listen(8081, function () {
-  console.log('success listen...8081');
+app.listen(8081, function() {
+	console.log('success listen...8081');
 });
