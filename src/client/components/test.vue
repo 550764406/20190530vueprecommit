ceb61307b68115e963853bc4ec56d76bcaa50db7
duanliwei <duanliwei@51kxd.cn>
2019-05-30 15:23:07 +0800

    ''

---
+++ src/client/components/test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div id="aa">
+      <input type="text" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    myclick() {
+      bb => {
+        console.log("");
+      };
+    }
+  }
+};
+</script>
+
+<style scoped></style>
