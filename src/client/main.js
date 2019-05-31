0cbf01da9315421376bfca159073e9e3a1c6bc65
duanliwei <duanliwei@51kxd.cn>
2019-05-31 11:59:13 +0800

    'test01'

--- src/client/main.js
+++ src/client/main.js
@@ -1,14 +1,18 @@
- import Vue from 'vue';
 
- import App from './App';
- import router from './router';
 
- Vue.config.productionTip = false;
 
- /* eslint-disable no-new */
- new Vue({
-   el: '#app',
-   router,
-   components: {App},
-   template: '<App/>'
- });
+
+        import Vue from 'vue';
+
+        import App from './App';
+        import router from './router';
+
+        Vue.config.productionTip = false;
+
+        /* eslint-disable no-new */
+        new Vue({
+          el: '#app',
+          router,
+          components: {App},
+          template: '<App/>'
+        });
