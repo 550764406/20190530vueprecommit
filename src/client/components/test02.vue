cfdb975be31c606d51fbd2cf8fe3ab1d553fcdf4
duanliwei <duanliwei@51kxd.cn>
2019-05-31 13:18:25 +0800

    'test02'

---
+++ src/client/components/test02.vue
@@ -0,0 +1,36 @@
+<template>
+  <div name="test02div">
+    <span class="test02span">
+      {{ msg }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test02',
+  data() {
+    return {
+      msg: 'test02'
+    };
+  },
+  methods: {
+    test02Fn() {
+      console.log('');
+    },
+    test02fn1() {
+      () => {
+        console.log('aaa');
+      };
+    },
+
+    test02fn2() {
+      (aa) => {
+        console.log('aaa11');
+      };
+    }
+  }
+};
+</script>
+
+<style scoped></style>
