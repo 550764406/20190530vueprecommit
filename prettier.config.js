27844066c524106618d8c03b81057fe854fc05a1
duanliwei <duanliwei@51kxd.cn>
2019-05-31 10:59:28 +0800

    'test01'

--- prettier.config.js
+++ prettier.config.js
@@ -16,13 +16,13 @@ module.exports = {
 
   trailingCooma: 'none', //在methods等方法或者函数结尾处 是否添加逗号， none：不添加  es5和all：都是添加逗号
 
-  bracketSpacing: false, //在对象字面量声明所使用的的花括号后（{）和前（}）输出空格
+  bracketSpacing: false, //在对象字面量声明所使用的的花括号后（{）和前（}）输出空格 true: 代表在花括号前后添加空格 false：不添加
 
   jsxBracketSameLine: false, // 在多行JSX元素最后一行的末尾（>）结束时  是否使用> 为单独一行
 
   arrowParens: 'always', //为单行箭头函数的参数是否添加圆括号 avoid | always
 
-  rangeStart: 0, //回退至包含选中语句的第一行的开始
+  rangeStart: 10, //回退至包含选中语句的第一行的开始
 
   rangeEnd: Infinity, //向前直到选中语句的末尾
 
