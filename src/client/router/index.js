e814cd9c040de42e4ad2be2c0731d2c501706fcf
duanliwei <duanliwei@51kxd.cn>
2019-05-31 13:28:32 +0800

    'test02'

--- src/client/router/index.js
+++ src/client/router/index.js
@@ -1,15 +1,1 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import HelloWorld from '@/client/components/HelloWorld';
-
-Vue.use(Router);
-
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-});
+                                              import Vue from 'vue';                                              import Router from 'vue-router';                                              import HelloWorld from '@/client/components/HelloWorld';                                              Vue.use(Router);                                              export default new Router({                                                routes: [                                                  {                                                    path: '/',                                                    name: 'HelloWorld',                                                    component: HelloWorld                                                  }                                                ]                                              });
