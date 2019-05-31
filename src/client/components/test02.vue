82e25c50b40b7512f9f9a560e79c6511f0fd3fdf
duanliwei <duanliwei@51kxd.cn>
2019-05-31 13:28:01 +0800

    'test02'

--- src/client/components/test02.vue
+++ src/client/components/test02.vue
@@ -1,36 +1,1 @@
-<template>
-  <div name="test02div">
-    <span class="test02span">
-      {{ msg }}
-    </span>
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'test02',
-  data() {
-    return {
-      msg: 'test02'
-    };
-  },
-  methods: {
-    test02Fn() {
-      console.log('');
-    },
-    test02fn1() {
-      () => {
-        console.log('aaa');
-      };
-    },
-
-    test02fn2() {
-      (aa) => {
-        console.log('aaa11');
-      };
-    }
-  }
-};
-</script>
-
-<style scoped></style>
+<template>  <div name="test02div">    <span class="test02span">      {{ msg }}    </span>  </div></template><script>export default {  name: 'test02',  data() {    return {      msg: 'test02'    };  },  methods: {    test02Fn() {      console.log('');    },    test02fn1() {      () => {        console.log('aaa');      };    },    test02fn2() {      (aa) => {        console.log('aaa11');      };    }  }};</script><style scoped></style>
