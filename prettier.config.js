976df58616697388120698319f18cbab90f75e67
duanliwei <duanliwei@51kxd.cn>
2019-05-31 10:54:09 +0800

    'test01'

--- prettier.config.js
+++ prettier.config.js
@@ -7,13 +7,14 @@ module.exports = {
 
   useTabs: false, //使用tab（制表位）缩进而非空格
 
-  semi: false, //在语句末尾添加分号, true: 添加分号   false: 不添加分号
+  semi: true, //在语句末尾添加分号, true: 添加分号   false: 如果是箭头函数，前面会添加；其他函数不会
 
   singleQuote: true, //用单引号替代双引号，true: 替代（这里vue里的不替换）  false：不替代
 
-  quoteProps: 'consistent',
+  quoteProps: 'consistent', // 这里没有测试出来
+  jsxSingleQuote: true, // jsx中用单引号替代双引号 true：代表替换  false：不替换
 
-  trailingCooma: 'none', //在任何可能的多行中输入尾逗号
+  trailingCooma: 'none', //在methods等方法或者函数结尾处 是否添加逗号， none：不添加  es5和all：都是添加逗号
 
   bracketSpacing: true, //在对象字面量声明所使用的的花括号后（{）和前（}）输出空格
 
@@ -30,4 +31,4 @@ module.exports = {
   insertPragma: false, //是否插入文件顶部的一些特殊的注释格式化代码
 
   proseWrap: 'preserve' //是否是当屏幕放不下时发生的软折行 always（当超出print width宽度时折行）|never|perserve(按照文件原样)
-}
+};
